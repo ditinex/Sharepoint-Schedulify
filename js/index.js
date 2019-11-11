acfde746ada147ba8162a461fe8327d8c1965cb1
Lazy Coderji <galaxyproscheduler@gmail.com>
2019-11-11 18:10:46 +0530

    imgage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 (function(){
 
+  var toggleSwitchStatus = true;
+
   var init = function(){
 
     chrome.runtime.sendMessage({cmd: 'app.getStatus'}, function(appStatus){
@@ -9,12 +11,19 @@
       var body = $('body');
       chrome.runtime.sendMessage({cmd: 'app.getPage'}, function(data){
         body.prepend(data);
+        toggleSwitchImg();
       });
 
     });
   };
 
-  function saveOptions(e) {
+  function toggleSwitchImg(){
+    closeimg = browser.runtime.getURL("img/close.png");
+    if(toggleSwitchStatus)
+      $('toggle-button').html('<img src="'+closeimg+'">')
+  }
+
+  function getDate() {
   	
   }
 
