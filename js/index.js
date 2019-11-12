ed7dc67a7b55230112ba5885b89623ad4ed617b2
Lazy Coderji <galaxyproscheduler@gmail.com>
2019-11-12 16:59:45 +0530

    panel design

--- js/index.js
+++ js/index.js
@@ -11,16 +11,38 @@
       var body = $('body');
       chrome.runtime.sendMessage({cmd: 'app.getPage'}, function(data){
         body.prepend(data);
+        initUI()
         toggleSwitchImg();
+        toggleSwitch();
       });
 
     });
   };
 
+  function initUI(){
+    $('#tabs').tabs({active: 0});
+  }
+
   function toggleSwitchImg(){
     closeimg = browser.runtime.getURL("img/close.png");
-    if(toggleSwitchStatus)
-      $('toggle-button').html('<img src="'+closeimg+'">')
+    openimg = browser.runtime.getURL("img/open.png");
+    if(toggleSwitchStatus){
+      $('.toggle-button').html('<img src="'+closeimg+'">');
+      $('.inject-container').removeClass('minimize');
+      $('#tabs').fadeIn(400);
+    }
+    else{
+      $('.toggle-button').html('<img src="'+openimg+'">');
+      $('.inject-container').addClass('minimize');
+      $('#tabs').fadeOut(400);
+    }
+  }
+
+  function toggleSwitch(){
+    $('.toggle-button').on('click',function() {
+      toggleSwitchStatus = !toggleSwitchStatus;
+      toggleSwitchImg();
+    });
   }
 
   function getDate() {
